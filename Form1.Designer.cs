d583db0ca979a100cc7b5e6c41f5f20deb32d857
poma <Semenov.Roman@mail.ru>
2015-03-06 14:12:55 +0300

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ToBase64 {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBox1.Location = new System.Drawing.Point(0, 0);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox1.Size = new System.Drawing.Size(514, 383);
+			this.textBox1.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(514, 383);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ToBase64 {
+	public partial class Form1 : Form {
+		public Form1(string filename) {
+			InitializeComponent();
+			OpenFile(filename);
+		}
+
+		private void OpenFile(string file) {
+			if (string.IsNullOrEmpty(file) || !File.Exists(file)) {
+				textBox1.Text = "";
+				Text = "Drag a file onto this window";
+				return;
+			}
+			var bytes = File.ReadAllBytes(file);
+			var base64 = Convert.ToBase64String(bytes);
+			Text = String.Format("{0}, {1}Kb -> {2}Kb", Path.GetFileName(file), bytes.Length / 1000, base64.Length / 1000);
+			Clipboard.SetText(base64);
+			textBox1.Text = base64.Length < 2000 ? base64 : "Too long to be displayed, copied to clipboard";
+		}
+
+		private void Form1_DragDrop(object sender, DragEventArgs e) {
+			var file = e.Data.GetData(DataFormats.FileDrop) as string[];
+			if (file != null && file.Length == 1 && File.Exists(file[0]))
+				OpenFile(file[0]);
+		}
+
+		private void Form1_DragEnter(object sender, DragEventArgs e) {
+			var file = e.Data.GetData(DataFormats.FileDrop) as string[];
+			if (file != null && file.Length == 1 && File.Exists(file[0]))
+				e.Effect = DragDropEffects.Move;
+		}
+	}
+}
